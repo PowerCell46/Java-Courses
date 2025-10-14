1df8bd666d17bab38c2e2ae5ac91f5c1875e9e99
Peter Gerdzhikov <peter.gerdzhikov.contact@gmail.com>
2025-10-14 21:06:03 +0300

    Add 3-4 more metricts

--- Investing/src/main/java/Main.java
+++ Investing/src/main/java/Main.java
@@ -11,6 +11,88 @@ public class Main {
 //        long netIncome = 111_068_000;
 //        long stockholdersEquity = 143_486_000;
 //        System.out.println(calculateReturnOnEquity(netIncome, stockholdersEquity));
+
+//        long operatingCashFlow = 121_527_000;
+//        long capitalExpenditure = 59_923_000;
+//        System.out.println(calculateFreeCashFlow(operatingCashFlow, capitalExpenditure));
+
+//        long totalDebt = 102_787_000;
+//        long stockholdersEquity = 143_486_000;
+//        System.out.println(calculateTotalDebtEquity(totalDebt, stockholdersEquity));
+
+//        long operatingIncome = 142_799_000;
+//        long revenue = 311_938_000;
+//        long netIncome = 111_068_000;
+//
+//        System.out.println(calculateOperatingMargin(operatingIncome, revenue));
+//        System.out.println(calculateNetMargin(netIncome, revenue));
+
+//        double sharePrice = 56.83;
+//        double earningsPerShare = 3.86;
+//
+//        System.out.println(calculatePriceToEarnings(sharePrice, earningsPerShare));
+    }
+
+    public static double calculatePriceToEarnings(double sharePrice, double earningsPerShare) {
+        // * Price to earnings (P/E)
+        // How much investors are willing to pay for each dollar of a company's earnings. Higher P/E: market expects stronger
+        // future growth; Lower P/E: undervaluation or weak prospects
+
+        // ! Yahoo finance: Statistics: Trailing P/E (ttm): uses last 12 months’ earnings (TTM = trailing twelve months).
+        // ! Yahoo finance: Statistics: Forward P/E: uses projected earnings for the next fiscal year.
+
+        double priceToEarnings = sharePrice / earningsPerShare;
+
+        return priceToEarnings; // * Investors pay "priceToEarnings" for every $1 of earnings.
+    }
+
+    public static double calculateNetMargin(long netIncome, long revenue) {
+        // * Profit margin (Net and operating)
+        // Measures how much of a company's revenue turns into profit - showing efficiency, pricing power, and cost control.
+        // - Net margin: what remains after all expenses, interest, and taxes
+
+        // ! Yahoo finance: Statistics: Profit margin
+
+        double netProfitMargin = ((double) netIncome / revenue) * 100;
+
+        return netProfitMargin;
+    }
+
+    public static double calculateOperatingMargin(long operatingIncome, long revenue) {
+        // * Profit margin (Net and operating)
+        // Measures how much of a company's revenue turns into profit - showing efficiency, pricing power, and cost control.
+        // - Operating margin: how much profit the core business generates before interest and taxes.
+
+        // ! Yahoo finance: Statistics: Operating margin
+
+        double operatingMargin = ((double) operatingIncome / revenue) * 100;
+
+        return operatingMargin;
+    }
+
+    public static double calculateTotalDebtEquity(long totalDebt, long stockholdersEquity) {
+        // * Debt to equity ratio (D/E)
+        // Shows how much a company relies on borrowed money/debt vs shareholder's capital (equity) to finance its operations.
+        // Higher ratios: greater dependence on debt and potentially higher risk
+
+        // ! Yahoo Finance: Statistics: Total Debt/Equity (mrq)
+
+        double totalDebtEquity = (double) totalDebt / stockholdersEquity;
+
+        return totalDebtEquity;
+    }
+
+    public static long calculateFreeCashFlow(long operatingCashFlow, long capitalExpenditure) {
+        // * Free cash flow
+        // Measures the actual cash a company generates after covering all its operating expenses and capital expenditures.
+        // It's one of the most important indicators of a company's financial health, flexibility, and ability
+        // to return value to shareholders (dividends, buybacks, reinvestment).
+
+        // ! Yahoo Finance: Financials -> Cash flow -> Free cash flow
+
+        long freeCashFlow = operatingCashFlow - capitalExpenditure; // capital expenditure is regularly a negative num
+
+        return freeCashFlow;
     }
 
     public static double calculateReturnOnEquity(long netIncome, long shareholdersEquity) {
