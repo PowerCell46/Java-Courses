4c80845f8589404c779c11720993ccc5ebea8ca2
Peter Gerdzhikov <peter.gerdzhikov.contact@gmail.com>
2025-12-14 23:04:37 +0200

    Adding exception handler to the management microservice

---
+++ ItCareerElevatorFourthExerciseManagementMicroservice/src/main/java/com/ItCareerElevator/ItCareerElevatorFourthExerciseManagementMicroservice/DTOs/response/ErrorResponseDTO.java
@@ -0,0 +1,19 @@
+package com.ItCareerElevator.ItCareerElevatorFourthExerciseManagementMicroservice.DTOs.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponseDTO {
+
+    private Integer status;
+
+    private String message;
+
+    private Long timestamp;
+}
---
+++ ItCareerElevatorFourthExerciseManagementMicroservice/src/main/java/com/ItCareerElevator/ItCareerElevatorFourthExerciseManagementMicroservice/controllers/ExceptionHandlerController.java
@@ -0,0 +1,93 @@
+package com.ItCareerElevator.ItCareerElevatorFourthExerciseManagementMicroservice.controllers;
+
+import com.ItCareerElevator.ItCareerElevatorFourthExerciseManagementMicroservice.DTOs.response.ErrorResponseDTO;
+import com.ItCareerElevator.ItCareerElevatorFourthExerciseManagementMicroservice.exceptions.InvalidFileImageException;
+import com.ItCareerElevator.ItCareerElevatorFourthExerciseManagementMicroservice.exceptions.InvalidLocalesException;
+import com.ItCareerElevator.ItCareerElevatorFourthExerciseManagementMicroservice.exceptions.NoSuchLocaleException;
+import com.ItCareerElevator.ItCareerElevatorFourthExerciseManagementMicroservice.exceptions.NoSuchProductException;
+import com.ItCareerElevator.ItCareerElevatorFourthExerciseManagementMicroservice.exceptions.ProductAlreadyExistsException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(InvalidFileImageException.class)
+    public ResponseEntity<ErrorResponseDTO> handleInvalidFileImageException(InvalidFileImageException ex) {
+        log.warn("Handling InvalidFileImageException.");
+
+        ErrorResponseDTO error = new ErrorResponseDTO(
+                HttpStatus.BAD_REQUEST.value(),
+                ex.getMessage(),
+                System.currentTimeMillis()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST.value())
+                .body(error);
+    }
+
+    @ExceptionHandler(InvalidLocalesException.class)
+    public ResponseEntity<ErrorResponseDTO> handleInvalidLocalesException(InvalidLocalesException ex) {
+        log.warn("Handling InvalidLocalesException.");
+
+        ErrorResponseDTO error = new ErrorResponseDTO(
+                HttpStatus.BAD_REQUEST.value(),
+                ex.getMessage(),
+                System.currentTimeMillis()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST.value())
+                .body(error);
+    }
+
+    @ExceptionHandler(NoSuchLocaleException.class)
+    public ResponseEntity<ErrorResponseDTO> handleNoSuchLocaleException(NoSuchLocaleException ex) {
+        log.warn("Handling NoSuchLocaleException.");
+
+        ErrorResponseDTO error = new ErrorResponseDTO(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                System.currentTimeMillis()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND.value())
+                .body(error);
+    }
+
+    @ExceptionHandler(NoSuchProductException.class)
+    public ResponseEntity<ErrorResponseDTO> handleNoSuchProductException(NoSuchProductException ex) {
+        log.warn("Handling NoSuchProductException.");
+
+        ErrorResponseDTO error = new ErrorResponseDTO(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                System.currentTimeMillis()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND.value())
+                .body(error);
+    }
+
+    @ExceptionHandler(ProductAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponseDTO> handleProductAlreadyExistsException(ProductAlreadyExistsException ex) {
+        log.warn("Handling ProductAlreadyExistsException.");
+
+        ErrorResponseDTO error = new ErrorResponseDTO(
+                HttpStatus.BAD_REQUEST.value(),
+                ex.getMessage(),
+                System.currentTimeMillis()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST.value())
+                .body(error);
+    }
+}
