0ecff379e91fd1ba8a6731358675a3cf03b503da
Peter Gerdzhikov <peter.gerdzhikov.contact@gmail.com>
2026-01-21 01:07:43 +0200

    Add db loader with init locales

--- ItCareerElevatorSixthExerciseProductService/src/main/java/com/ItCareerElevatorSixthExercise/CommandLineRunnerImpl.java
+++ ItCareerElevatorSixthExerciseProductService/src/main/java/com/ItCareerElevatorSixthExercise/CommandLineRunnerImpl.java
@@ -2,8 +2,10 @@ package com.ItCareerElevatorSixthExercise;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+@Order(2)
 @Component
 @RequiredArgsConstructor
 public class CommandLineRunnerImpl implements CommandLineRunner {
---
+++ ItCareerElevatorSixthExerciseProductService/src/main/java/com/ItCareerElevatorSixthExercise/DatabaseLoader.java
@@ -0,0 +1,40 @@
+package com.ItCareerElevatorSixthExercise;
+
+import com.ItCareerElevatorSixthExercise.entities.Locale;
+import com.ItCareerElevatorSixthExercise.repositories.LocaleRepository;
+import com.ItCareerElevatorSixthExercise.services.interfaces.LocaleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Slf4j
+@Order(1)
+@Component
+@RequiredArgsConstructor
+public class DatabaseLoader implements CommandLineRunner {
+
+    private final LocaleRepository localeRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (localeRepository.findAll().isEmpty()) {
+            seedLocales();
+        }
+    }
+
+    private void seedLocales() {
+        Locale bulgarian = new Locale("bg", "Bulgarian");
+        Locale english = new Locale("en", "English");
+        Locale spanish = new Locale("es", "Spanish");
+        Locale german = new Locale("de", "German");
+
+        Set<Locale> locales = Set.of(bulgarian, english, spanish, german);
+
+        log.info("Persisting {} locales to the database.", locales.size());
+        localeRepository.saveAll(locales);
+    }
+}
